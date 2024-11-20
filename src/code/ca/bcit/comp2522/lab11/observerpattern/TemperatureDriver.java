2761a22968ddc22a16efd13eadbbfa9a61f2856c
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-11-19 18:47:44 -0800

    Commented TemperatureDriver.

--- src/code/ca/bcit/comp2522/lab11/observerpattern/TemperatureDriver.java
+++ src/code/ca/bcit/comp2522/lab11/observerpattern/TemperatureDriver.java
@@ -1,32 +1,62 @@
 package ca.bcit.comp2522.lab11.observerpattern;
 
+/**
+ * The entry point for the temperature tracking application.
+ * This class demonstrates the Observer design pattern by tracking temperature
+ * and notifying various observers about changes.
+ *
+ * <p>
+ * Observers:
+ * <ul>
+ *   <li>FurnaceController</li>
+ *   <li>HeatwaveMonitor</li>
+ *   <li>RoadConditionAdvisor</li>
+ *   <li>FurnaceUsageMonitor (via Java Observer)</li>
+ * </ul>
+ *
+ * Observables:
+ * <ul>
+ *   <li>TemperatureTracker</li>
+ *   <li>FurnaceController (via Java Observable)</li>
+ * </ul>
+ */
 public class TemperatureDriver {
+
+    /**
+     * The main method where the program starts execution.
+     * It initializes the TemperatureTracker and various observers,
+     * attaches the observers to the tracker, and simulates temperature readings
+     * in different units.
+     *
+     * @param args command-line arguments (not used).
+     */
     public static void main(final String[] args) {
         // Custom Observable
         final TemperatureTracker tempTracker;
 
         // Custom Observers
-        final FurnaceController furnaceControllerCustom;
+
+        // FurnaceController Implements Java Observable
+        final FurnaceController furnaceController;
         final HeatwaveMonitor heatwaveMonitor;
         final RoadConditionAdvisor roadAdvisor;
 
-        // Implements Java Observable
-        final FurnaceController furnaceController;
-        // Implements Java Observer
+        // FurnaceUsageMonitor Implements Java Observer
         final FurnaceUsageMonitor furnaceUsageMonitor;
 
+        // Initialize the TemperatureTracker (observable) and observers
         tempTracker = new TemperatureTracker();
-        furnaceControllerCustom = new FurnaceController(20);
+        furnaceController = new FurnaceController(20);
         heatwaveMonitor = new HeatwaveMonitor(40);
         roadAdvisor = new RoadConditionAdvisor(-1);
 
-        tempTracker.addTemperatureProcessor(furnaceControllerCustom);
+        // Attach observers to the TemperatureTracker
+        tempTracker.addTemperatureProcessor(furnaceController);
         tempTracker.addTemperatureProcessor(heatwaveMonitor);
         tempTracker.addTemperatureProcessor(roadAdvisor);
 
-        furnaceController = new FurnaceController(25);
+        // Attach FurnaceUsageMonitor to FurnaceController
         furnaceUsageMonitor = new FurnaceUsageMonitor();
-
         furnaceController.addObserver(furnaceUsageMonitor);
 
         // Trigger temperature readings to test all observers
